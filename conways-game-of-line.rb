2b6ff4c3648c40ba482e72af5a67186a3fb246a3
Guillermo Siliceo <guillermo.siliceo@gmail.com>
2021-12-28 22:42:21 -0600

    Support ruby 2

--- conways-game-of-line.rb
+++ conways-game-of-line.rb
@@ -1,3 +1,18 @@
+# -*- encoding : utf-8 -*-
+class Array
+  def tally_by(&function)
+    function ||= -> v { v }
+
+    each_with_object(Hash.new(0)) do |value, hash|
+      hash[function.call(value)] += 1
+    end
+  end
+
+  def tally
+    tally_by(&:itself)
+  end
+end
+
 class LifeSimulator
   def initialize(seed)
     @seed = seed
